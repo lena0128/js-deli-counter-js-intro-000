725fc40e34ab5fe66c7e76b731e7fe9c0d0f1fbe
Lena Hou <lenahou2013@gmail.com>
2020-12-13 15:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length > 0) {
     var newLine = [];
     for(let i = 0; i < katzDeliLine.length; i++){
-   newLine.slice(`${i+1}. ${katzDeliLine[i]}`);
+   newLine += newLine(`${i+1}. ${katzDeliLine[i]}`);
  }
  return `The line is currently: ${newLine}`;
   } else {
